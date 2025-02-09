319a9aa7ce3ace7400923b11264a0fb33a750bd5
ink-song <229135518@qq.com>
2025-02-09 21:39:41 +0800

    feat: 添加ui框架

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
 export default function Home() {
-  return <div>Hello Next !</div>;
+  return (
+    <div>
+      <span>12312</span>
+      <Button>1111</Button>
+    </div>
+  );
 }

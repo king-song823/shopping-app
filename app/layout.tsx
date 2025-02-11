d8b5f2982085f9762428c2519e7b790759618edf
ink-song <229135518@qq.com>
2025-02-11 21:51:56 +0800

    feat: add theme mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/assets/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -12,6 +13,7 @@ export const metadata: Metadata = {
   description: APP_DESCRIPTION,
   metadataBase: new URL(SERVER_URL),
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -19,7 +21,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }

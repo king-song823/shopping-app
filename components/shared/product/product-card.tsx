67118441eabdd93068c9ff5eb2df9b9c9cc9ad44
ink-song <229135518@qq.com>
2025-02-17 15:36:55 +0800

    feat: add zod validator & generate types

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,9 +1,9 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import Link from "next/link";
-import Image from "next/image";
-import ProductPrice from "@/components/shared/product/product-price";
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const ProductCard = ({ product }: { product: any }) => {
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import Link from 'next/link';
+import Image from 'next/image';
+import ProductPrice from '@/components/shared/product/product-price';
+import { Product } from '@/types';
+const ProductCard = ({ product }: { product: Product }) => {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader className="p-0 items-center">

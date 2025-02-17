9a9f6085fc144dfcab5bd6b8bef6a6d5acb45278
ink-song <229135518@qq.com>
2025-02-17 16:13:29 +0800

    feat: serverless-env-config

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,6 @@ import ProductList from '@/components/shared/product/product-list';
 import { getLastestProdcuts } from '@/lib/actions/product.actions';
 export default async function Home() {
   const lastestProdcuts = await getLastestProdcuts();
-  console.log('lastestProdcuts', lastestProdcuts);
   return (
     <div className="space-y-8">
       <h2 className="h2-bold">Latest Products</h2>
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,11 +1,10 @@
 'use server';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/db/prisma';
 import { converToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 // Get the lastest products
 export async function getLastestProdcuts() {
-  const prisma = new PrismaClient();
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: {

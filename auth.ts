bc702751c99d65ca162fdd967251af49105be776
ink-song <229135518@qq.com>
2025-02-20 13:37:19 +0800

    chore: test

--- auth.ts
+++ auth.ts
@@ -1,5 +1,5 @@
 // import { compareSync } from 'bcrypt-ts-edge';
-// import NextAuth from 'next-auth';
+import NextAuth from 'next-auth';
 // import CredentialsProvider from 'next-auth/providers/credentials';
 
 // import { prisma } from '@/db/prisma';

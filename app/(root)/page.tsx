c71c1d0d2dcad587ceea64fdf19fbe5bd153ab96
ink-song <229135518@qq.com>
2025-02-27 15:28:25 +0800

    feat: create a carousel banner

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
 import ProductList from '@/components/shared/product/product-list';
 import { getLastestProdcuts } from '@/lib/actions/product.actions';
+import { getFeaturedProducts } from '@/lib/actions/product.actions';
+import ProductCarousel from '@/components/shared/product/product-carousel';
 export default async function Home() {
   const lastestProdcuts = await getLastestProdcuts();
+  const featuredProducts = await getFeaturedProducts();
   return (
     <div className="space-y-8">
-      <h2 className="h2-bold">Latest Products</h2>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList title="Newest Arrivals" data={lastestProdcuts} />
     </div>
   );
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -148,3 +148,17 @@ export async function getAllCategories() {
   });
   return data;
 }
+
+// Get featured products
+export async function getFeaturedProducts() {
+  const data = await prisma.product.findMany({
+    where: {
+      isFeatured: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    take: 4,
+  });
+  return converToPlainObject(data);
+}

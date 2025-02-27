6dd075c534f2ec2f706b6d7c337cdd180205de0a
ink-song <229135518@qq.com>
2025-02-27 10:38:52 +0800

    feat: create admin user page & update and delete user

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,12 +6,15 @@ import {
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
+  updateUserSchema,
 } from '../validator';
 import { hashSync } from 'bcrypt-ts-edge';
 import { prisma } from '@/db/prisma';
-import { formatError } from '../utils';
+import { formatError, handleError } from '../utils';
 import { ShippingAddress } from '@/types';
 import { z } from 'zod';
+import { PAGE_SIZE } from '../constants';
+import { revalidatePath } from 'next/cache';
 // Sign in the user with credentials
 export async function signInWithCredentials(_: unknown, formData: FormData) {
   try {
@@ -169,6 +172,66 @@ export async function updateProfile(user: { name: string; email: string }) {
         name: user.name,
       },
     });
+
+    return {
+      success: true,
+      message: 'User updated successfully',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Get all users
+export async function getAllUsers({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const data = await prisma.user.findMany({
+    orderBy: { createdAt: 'desc' },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const dataCount = await prisma.user.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
+}
+
+// Delete user by ID
+export async function deleteUser(id: string) {
+  try {
+    await prisma.user.delete({ where: { id } });
+
+    revalidatePath('/admin/users');
+
+    return {
+      success: true,
+      message: 'User deleted successfully',
+    };
+  } catch (error) {
+    return handleError(error);
+  }
+}
+
+// Update user
+export async function updateUser(user: z.infer<typeof updateUserSchema>) {
+  try {
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        name: user.name,
+        role: user.role,
+      },
+    });
+
+    revalidatePath('/admin/users');
 
     return {
       success: true,
--- lib/validator.ts
+++ lib/validator.ts
@@ -125,8 +125,13 @@ export const updateProfileSchema = z.object({
   email: z.string().min(3, 'Email must be at least 3 characters'),
 });
 
-//Schema for updating a product
-
+// Schema for updating a product
 export const updateProductSchema = insertProductSchema.extend({
   id: z.string().min(1, 'Id is required'),
+});
+
+// Update User Schema
+export const updateUserSchema = updateProfileSchema.extend({
+  id: z.string().min(1, 'Id is required'),
+  role: z.string().min(1, 'Role is required'),
 });
